c38662d77737b9e94880a92bd62910809b64d5d1
hnikoloski <h.nikoloski@outlook.com>
2022-02-17 14:56:08 +0100

    last update ne update

--- archive-events.php
+++ archive-events.php
@@ -12,12 +12,10 @@ get_header();
 ?>
 
 <header class="single-page-header page-padding-x d-flex flex-wrap">
-    <h2 class="heading blue-bg w-fit-content"><?php _e('Upcoming Events', 'starter'); ?></h2>
+    <h2 class="heading red-bg w-fit-content"><?php _e('Upcoming Events', 'starter'); ?></h2>
 </header>
-<main id="primary" class="site-main page-padding-x">
+<main id="primary" class="site-main">
     <div class="container">
-
-
         <?php
         // WP_Query arguments
         $args = array(
@@ -37,46 +35,46 @@ get_header();
         // The Loop
         if ($query->have_posts()) {
         ?>
-            <div class="row">
-                <div class="col col-title">
-                    <h3><?php _e('Title', 'starter'); ?></h3>
-                </div>
-                <div class="col col-date">
-                    <h3><?php _e('Date', 'starter'); ?></h3>
-                </div>
-                <div class="col col-img">
-                    <h3><?php _e('Poster', 'starter'); ?></h3>
-                </div>
-                <div class="col col-btn">
-                    <h3><?php _e('Event Info', 'starter'); ?></h3>
-                </div>
-            </div>
+
             <?php
             while ($query->have_posts()) {
                 $query->the_post();
             ?>
-                <div class="row" data-link="<?php the_permalink(); ?>">
-                    <div class="col col-title">
-                        <h4><?php the_title(); ?></h4>
-                    </div>
+                <div class="row">
                     <div class="col col-date">
-                        <h5><?php
-                            if (get_field('event_date')) {
-                                $theDate = get_field('event_date');
-                                echo date('M j, Y', strtotime($theDate));
-                            } else {
-                                echo 'TBD';
-                            }
+                        <p class="day">
+                            <?php
+                            $event_date = get_field('event_date');
+                            $event_date = new DateTime($event_date);
+                            echo $event_date->format('D');
                             ?>
-                        </h5>
+                        </p>
+                        <p class="day-num">
+                            <?php
+                            $event_date = get_field('event_date');
+                            $event_date = new DateTime($event_date);
+                            echo $event_date->format('d');
+                            ?>
+                        </p>
+                    </div>
+                    <div class="col col-content">
+                        <?php if (get_field('event_time')) : ?>
+                            <p class="time">
+                                <i class="far fa-light fa-clock"></i> <?php the_field('event_time'); ?>
+                            </p>
+                        <?php endif; ?>
+                        <h3 class="heading"><?php the_title(); ?></h3>
+                        <p>
+                            <?php
+                            echo wp_trim_words(get_the_excerpt(), 55);
+                            ?>
+                        </p>
+                        <a href="<?php the_permalink(); ?>" class="btn btn-blank"><?php _e('View More'); ?> <i class="fal fa-long-arrow-right"></i></a>
                     </div>
                     <div class="col col-img">
                         <div class="img-wrapper">
-                            <img src="<?= get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="full-size-img full-size-img-cover">
+                            <img src="<?php the_post_thumbnail_url(); ?>" class="full-size-img full-size-img-cover">
                         </div>
-                    </div>
-                    <div class="col col-btn">
-                        <a href="<?php the_permalink(); ?>" class="btn btn-blank mx-auto"><?php _e('View More', 'starter'); ?><i class="fal fa-long-arrow-right"></i></a>
                     </div>
                 </div>
             <?php
@@ -87,14 +85,15 @@ get_header();
                 <div class="col col-full">
                     <h4><?php _e('No upcoming events', 'starter'); ?></h4>
                 </div>
-            <?php
+            </div>
+        <?php
         }
 
         // Restore original Post Data
         wp_reset_postdata();
 
-            ?>
-            </div>
+        ?>
+    </div>
 </main><!-- #main -->
 
 <?php
